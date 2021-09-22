f08496836f3fef85b40ceb107e27ceda48873542
John Galloway <john.t.galloway@gmail.com>
2021-09-21 20:17:37 -0500

    Added Question 3 and 7 for chapter 3

---
+++ Module 2 Assessments/Chapter 3 Quesion 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter_3_Quesion_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(7.31 % 10);
+        }
+    }
+}
---
+++ Module 2 Assessments/Module 2 Assessments/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Module_2_Assessments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputnum;
+            Console.Write("Enter an Integer of at least 3 digits long: ");
+            bool validint = int.TryParse(Console.ReadLine(), out inputnum);
+            while (validint == false)
+            { 
+                Console.WriteLine("Please enter valid Integer: ");
+                validint = int.TryParse(Console.ReadLine(), out inputnum);                           
+            }
+            int num1 = (inputnum / 100);
+            decimal num2 = num1;
+            num2 = decimal.Round(num1);
+            if (num2 == 7)
+                Console.WriteLine("There is a 7 third from the right");
+            else
+                Console.WriteLine("There is NOT a 7 third from the right");
+
+
+
+
+
+
+        }
+    }
+}
